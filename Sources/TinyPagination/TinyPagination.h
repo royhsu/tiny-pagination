5672201d88c88466f15b47ead970b3dc7af4a58b
Roy Hsu <roy.hsu@tinyworld.cc>
2019-11-18 16:45:51 +0800

    add missing files.

---
+++ Sources/TinyPagination/TinyPagination.h
@@ -0,0 +1,13 @@
+//
+//  TinyPagination.h
+//  TinyPagination
+//
+//  Created by Roy Hsu on 18/11/2019.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double TinyPaginationVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TinyPaginationVersionString[];
