3bfbc06a6553f108c1a7e7043d070aae7e060d11
Roy Hsu <roy.hsu@tinyworld.cc>
2019-11-27 20:54:08 +0800

    minor.

--- Tests/TinyPaginationTests/XCTestManifests.swift
+++ Tests/TinyPaginationTests/XCTestManifests.swift
@@ -1,11 +1,14 @@
 import XCTest
 
 #if !canImport(ObjectiveC)
+
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(MessageServiceTests.allTests),
+    
+    [
         testCase(PageManagerTests.allTests),
         testCase(TableTests.allTests),
     ]
+    
 }
+
 #endif
